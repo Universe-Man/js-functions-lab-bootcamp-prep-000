e680bbe0167b297cd780c133acc0836150714f6b
Ian Pollack <ianjpollack@gmail.com>
2018-01-07 07:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}`
 }
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name!}`
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
-  return "It's 20 days until Mother's Day!"
+  return `It's ${20} days until ${holiday}!`
 }
