9827db3514e10dd49adec6feca69002e6e0fccf7
Ian Pollack <ianjpollack@gmail.com>
2018-01-07 07:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
-function happyHolidayTo() {
+function happyHolidayTo(name) {
   return "Happy $[holiday}, ${name}!"
 }
