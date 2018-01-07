9c837b05733ec9432fb4389478fb439c7bacc589
Ian Pollack <ianjpollack@gmail.com>
2018-01-07 07:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(name) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown() {
-  return "It's ${days} days until ${holiday}!"
+  return "It's 20 days until Mother's Day!"
 }
